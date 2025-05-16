1f419eb7e757e7e52bfe75be0877cb60539895fc
Procols <connect.procols@gmail.com>
2025-05-17 02:46:53 +0530

    Update settings.py

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -119,17 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    '/myapp/static/'
+    BASE_DIR / 'myapp' / 'static',
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
-MEDIA_URL='/myapp/media/'
-MEDIA_ROOT=BASE_DIR/'myapp/media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'myapp' / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
