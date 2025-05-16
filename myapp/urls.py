aa805444e59e4841a182a20c994806b86bc66cd4
Ajay Raj <ajaylogu64@gmail.com>
2025-05-16 18:17:04 +0530

    front base2

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.base, name='home'),
     path('about', views.about, name='about'),
-    path('categories', views.categories, name='categories'),
-    path('products', views.products, name='products'),
+    path('categories/', views.categories, name='categories'),
+    path('products/', views.products, name='products'),
     
     
 ]
