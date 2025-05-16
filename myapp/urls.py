0fa66ba30a2894a492f7ac3fc282a27257d191c6
Ajay Raj <ajaylogu64@gmail.com>
2025-05-17 02:12:11 +0530

    full demo

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='home'),
-    path('about', views.about, name='about'),
+    path('about/', views.about, name='about'),
     path('categories/', views.categories, name='categories'),
     path('products/', views.products, name='products'),
+    path('signin', views.signin, name='signin'),
+    path('product_details/', views.product_details, name='product_details'),
+    path('mycart/', views.mycart, name='mycart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('myorder/', views.myorder, name='myorder'),
+    path('watchlist/', views.watchlist, name='watchlist'),
     
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -12,3 +12,22 @@ def categories(request):
 
 def products(request):
     return render(request, 'pages/products.html')
+
+def signin(request):
+    return render(request, 'pages/signin.html')
+
+def product_details(request):
+    return render(request, 'pages/product_details.html')
+
+def mycart(request):
+    return render(request, 'pages/mycart.html')
+
+def checkout(request):
+    return render(request, 'pages/checkout.html')
+
+def myorder(request):
+    return render(request, 'pages/myorder.html')
+
+def watchlist(request):     
+    return render(request, 'pages/watchlist.html')
+
